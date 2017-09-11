a27dbb5e9ed15c5e32326f61745af25bb1cfb4b5
Olesea Harea <olesea.harea@committed.ro>
2017-09-11 15:14:41 +0300

    adds method to return correct currency name

--- src/app/user-request/inventory.model.ts
+++ src/app/user-request/inventory.model.ts
@@ -3,14 +3,4 @@ export class Inventory {
   totalSum: number;
   denominations: number[];
   currency: string;
-
-  centsToPound(amount) {
-    if (amount >= 100) {
-      amount = amount/100 + 'pound';
-      console.log('the amount is', amount);
-      return amount;
-    } else {
-      return amount;
-    }
-  }
 }
--- src/app/user-request/user-request.component.ts
+++ src/app/user-request/user-request.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Inventory } from './inventory.model';
 
 @Component({
@@ -9,20 +9,50 @@ import { Inventory } from './inventory.model';
 export class UserRequestComponent {
 
   public inventory: Inventory;
+  private totalCoinsReturned: number;
+  private specificCoinsReturned: number;
+  private currencyDenomination: number;
+  private currencyName: string;
+
   private afterSubmit: boolean = false;
 
   constructor() {
     this.inventory = new Inventory();
-    this.inventory.denominations  = [1, 2, 5, 10, 20, 50, 100];
+    this.inventory.denominations = [1, 2, 5, 10, 20, 50, 100];
     this.inventory.currency = 'cent';
   }
 
-  calculateOptimalChange(input: number) :number  {
+  //sets correct name for currency
+  setCurrency(amount: any): string {
+    this.currencyName = this.inventory.currency;
+    if(amount > 1) {
+      this.currencyName = 'cents';
+      console.log('currency must be set to cents ', this.currencyName);
+    }
+    if(amount >= 100) {
+      amount = amount/100;
+      this.currencyName = 'pounds';
+      console.log('currency must be set to pounds ', this.currencyName);
+
+    }
+    return amount + this.currencyName;
+  }
+
+  calculateOptimalChange(reqSum: HTMLInputElement): any  {
     this.afterSubmit = true;
 
-    let change = this.inventory.centsToPound(input);
-    console.log('this is the input',  this.inventory);
-    return change;
+    //implement logic for giving change
+    let denoms = this.inventory.denominations;
+
+
+
+    //convert currency
+    if(reqSum) {
+      let converted = this.setCurrency(reqSum.value);
+      console.log('converted currency', converted);
+      return converted;
+    }
+
   }
 
 }
