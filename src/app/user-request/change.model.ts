89383b327aa2296abfa7c4354ec813f1f806fee6
Olesea Harea <olesea.harea@committed.ro>
2017-09-12 17:09:43 +0300

    adds change model

---
+++ src/app/user-request/change.model.ts
@@ -0,0 +1,10 @@
+
+export class Change {
+  coinsNumber: number;
+  coinsDenoms: number;
+
+  constructor(coinsNumber, coinsDenoms){
+    this.coinsNumber = coinsNumber;
+    this.coinsDenoms = coinsDenoms;
+  }
+}
